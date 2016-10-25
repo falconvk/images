1156d3444031351f75ac79fac9cedd14af6ccbc6
Nikola Rajkovic <falconvk@gmail.com>
2016-10-25 02:20:10 +0200

    upgrade App to CBC and fetch images

--- client/main.js
+++ client/main.js
@@ -1,25 +1,38 @@
 // Any JS in here is automatically ran for us
 
 // Import the React library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import ImageList from './components/image_list';
 
 // Create a component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
+  }
+
+  componentWillMount() {
+    // load data
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data }));
+  }
+
+  render() {
+    console.log(this.state.images);
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
+}
 
 
 // Render this component to the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
-  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-    .then(response => console.log(response));
 });
